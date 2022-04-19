9e45efdd9ac025221c92e24fac5ef0f45c256d94
yasinozmen <81865185+yasinozmen@users.noreply.github.com>
2022-04-20 01:27:21 +0300

    .

--- springbootkodlar/src/test/java/javaaws/javaodev/springboot/SpringbootAWSWebAppApplicationTests.java
+++ springbootkodlar/src/test/java/javaaws/javaodev/springboot/SpringbootAWSWebAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootThymeleafCrudWebAppApplicationTests {
+class SpringbootAWSWebAppApplicationTests {
 
 	@Test
 	void contextLoads() {
